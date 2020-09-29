cd7b0109ff96c40dc8f1ffd562c5b9af899424de
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-09-28 21:43:32 -0500

    wireframes added

--- src/BookDetails/BookDetails.js
+++ src/BookDetails/BookDetails.js
@@ -1,1 +1,38 @@
+import React, { Component } from 'react';
+
 //This page should display a single books details when clicked on it should also contain a delete function to remove the book from the list
+export default class BookDetails extends Component {
+	state = {
+		wish: false,
+		title: '',
+		author: '',
+		genere: '',
+		lent: false,
+		completed: false,
+	};
+
+	Display = () => {
+		if (this.state.wish) {
+			return (
+				<div>
+					<div id="Title">Title: </div>
+					<div id="Author">Author: </div>
+					<div id="Genere">Genere: </div>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<div id="Title">Title: </div>
+					<div id="Author">Author: </div>
+					<div id="Genere">Genere: </div>
+					<div id="Lent">Lent Out: </div>
+					<div id="Completed">Completed: </div>
+				</div>
+			);
+		}
+	};
+	render() {
+		return <div>{this.Display}</div>;
+	}
+}
--- src/BookEntry/BookEntry.js
+++ src/BookEntry/BookEntry.js
@@ -1,1 +1,49 @@
+import React, { Component } from 'react';
+
 //This component should return a form with entry information and take a single prop that determines if the book is entered into the normal or the wish list.
+export default class BookEntry extends Component {
+	state = {
+		wish: false,
+		title: '',
+		author: '',
+		genere: '',
+		lent: false,
+		completed: false,
+	};
+
+	Display = () => {
+		console.log(this.state.wish);
+		if (this.state.wish) {
+			return (
+				<form>
+					<label htmlFor="Title">Title: </label>
+					<input type="text" id="Title" />
+					<label htmlFor="Author">Author: </label>
+					<input type="text" id="Author" />
+					<label htmlFor="Genere">Genere: </label>
+					{/* Drop down list of Genere Options */}
+					<button type="submit">Submit</button>
+				</form>
+			);
+		} else {
+			return (
+				<form>
+					<label htmlFor="Title">Title: </label>
+					<input type="text" id="Title" />
+					<label htmlFor="Author">Author: </label>
+					<input type="text" id="Author" />
+					<label htmlFor="Genere">Genere: </label>
+					{/* Drop down list of Genere Options */}
+					<label htmlFor="Lent">Lent Out: </label>
+					{/* Check Box */}
+					<label htmlFor="Completed">Completed: </label>
+					{/* Check Box */}
+					<button type="submit">Submit</button>
+				</form>
+			);
+		}
+	};
+	render() {
+		return <div>{this.Display}</div>;
+	}
+}
